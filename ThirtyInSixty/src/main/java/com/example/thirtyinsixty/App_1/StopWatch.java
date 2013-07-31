f9437c2c42230a7b84c0a6a4033d49ce6c8c55ca
kaghaffa <kaghaffa@gmail.com>
2013-07-30 20:16:20 -0700

    Started coding up StopWatch class

--- ThirtyInSixty/src/main/java/com/example/thirtyinsixty/App_1/StopWatch.java
+++ ThirtyInSixty/src/main/java/com/example/thirtyinsixty/App_1/StopWatch.java
@@ -7,7 +7,62 @@ public class StopWatch {
 
     private long startTime;
     private long endTime;
+    private long pauseOffset
     private boolean isRunning;
 
 
+    public StopWatch() {
+        reset();
+    }
+
+    public void start() {
+        if (!isRunning) {
+            startTime = System.currentTimeMillis();
+            isRunning = true;
+        }
+    }
+
+    public void stop() {
+        if (isRunning) {
+            endTime = System.currentTimeMillis();
+            pauseOffset = getElapsedTime();
+            isRunning = false;
+        }
+    }
+
+    public void reset() {
+        startTime = 0;
+        endTime = 0;
+        pauseOffset = 0;
+        isRunning = false;
+    }
+
+
+    public long getElapsedTime() {
+        if (isRunning) {
+            return System.currentTimeMillis() - startTime;
+        } else {
+            return System.currentTimeMillis() - startTime - pauseOffset;
+        }
+    }
+
+
+    public String getTimeAsString() {
+        long time = getElapsedTime();
+        long
+    }
+
+
+    public long getStartTime() {
+        return startTime;
+    }
+
+
+    public long getEndTime() {
+        return endTime;
+    }
+
+    public boolean isRunning() {
+        return isRunning;
+    }
 }
